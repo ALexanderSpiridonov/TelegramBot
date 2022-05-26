e80595bcf994c1dfa033638c5dd52d7e6702792c
Alexander Spiridonov <spiridonov.alexandr@gmail.com>
2022-05-26 19:20:19 +0200

    wip

--- bot.py
+++ bot.py
@@ -16,35 +16,39 @@ def send_welcome(message):
 
 # one word check
 @bot.message_handler(func=lambda m: len(m.text.split()) > 0) #, regexp="[^А-Яа-яa0-9]+")
-def echo_all(message):
+def reply_one_word(message):
     text = message.text # + " "
     text_clean = re.sub('[^А-Яа-яa0-9]+', ' ', text)
+    
+    # bot replies
+    if any(val in text_clean.lower().split() for val in ["триста", "300"]):
+        bot.reply_to(message, "отсоси у тракториса")
 
-    if len(text_clean.split()) > 0:
-        # bot replies
-        if any(val in text_clean.lower().split() for val in ["триста", "300"]):
-            bot.reply_to(message, "отсоси у тракториса")
+    elif text_clean.lower().split()[-1] in ["да", "дa"]:
+        bot.reply_to(message, "пизда")
 
-        elif text_clean.lower().split()[-1] in ["да", "дa"]:
-            bot.reply_to(message, "пизда")
+    elif text_clean.lower().split()[-1] in ["нет", "нeт", "net"]:
+        bot.reply_to(message, "пидора ответ")
 
-        elif text_clean.lower().split()[-1] in ["нет", "нeт", "net"]:
-            bot.reply_to(message, "пидора ответ")
+    elif "наверно" in text_clean.lower().split() :
+        bot.send_sticker(chat_id, file_id)
 
-        elif "наверно" in text_clean.lower().split() :
-            bot.send_sticker(chat_id, file_id)
+    elif "получилось" in text_clean.lower().split()[-1]:
+        bot.reply_to(message, "рубаха в жопу засучилась")
 
-        elif "получилось" in text_clean.lower().split()[-1]:
-            bot.reply_to(message, "рубаха в жопу засучилась")
+    elif "получается" in text_clean.lower().split()[-1]:
+        bot.reply_to(message, "... и хуй стоит, и голова качается!")
 
-        elif "получается" in text_clean.lower().split()[-1]:
-            bot.reply_to(message, "... и хуй стоит, и голова качается!")
-
-    # if len(text_clean.split()) > 1:
-    #     if "дай мне" in ' '.join(text_clean.lower().split[-2:]):
-    #         bot.reply_to(message, "... у тебя рука в говне")
-        
-    #     elif "будь другом" in ' '.join(text_clean.lower().split[-2:]):
-    #         bot.reply_to(message, "насри кругом!")
-        
+# more than two words messages
+@bot.message_handler(func=lambda m: len(m.text.split()) > 1)
+def reply_two_words(message):
+    text = message.text # + " "
+    text_clean = re.sub('[^А-Яа-яa0-9]+', ' ', text)
+    
+    if "дай мне" in ' '.join(text_clean.lower().split[-2:]):
+        bot.reply_to(message, "... у тебя рука в говне")
+    
+    elif "будь другом" in ' '.join(text_clean.lower().split[-2:]):
+        bot.reply_to(message, "насри кругом!")
+    
 bot.polling()
