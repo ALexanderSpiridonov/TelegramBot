c8c90050700dfcc6c17b6f5742a26a39546db99a
Alexander Spiridonov <spiridonov.alexandr@gmail.com>
2022-05-27 22:52:43 +0200

    send random sticker after 3 sec of silence

--- bot.py
+++ bot.py
@@ -2,17 +2,51 @@
 
 import os
 import re
+import random
 import telebot
-# from config import BOT_TOKEN
+from threading import Timer
+from config import stickers_id
+
+
+def send_random_sticker():
+    rand_int = random.randint(0, len(stickers_id))
+    random_sticker_id = stickers_id[rand_int]
+    bot.send_sticker(chat_id, random_sticker_id)
+
+def newTimer():
+    global t
+    t = Timer(5.0, send_random_sticker)
+newTimer()
+
+
+## production 
 BOT_TOKEN = os.environ['BOT_TOKEN']
 chat_id = os.environ['CHAT_ID']
+
+###### test bot
+# BOT_TOKEN = "574632910:AAFWAN7buAcQe7MNsmu2VnQGCtFYJJh0Ewc"
+# chat_id = 76016759
+
+# initiat bot
 bot = telebot.TeleBot(BOT_TOKEN)
 
+# sticker for "наверное"
 file_id = "CAACAgIAAx0CZ7OvuQACP45ij2-WhDPJX8fDzzizrxzz7iXV4AAC2hAAAuZ4kEoD72yHqENwoSQE"
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Скажи триста")
+
+# set up timer for messages
+@bot.message_handler(func=lambda m: True)
+def send_refresh(message):
+    # print('reset timer and start again')
+    t.cancel()
+    newTimer()
+    t.start()
+    # print("\n timer started")
+    
 
 # one word check
 @bot.message_handler(func=lambda m: len(m.text.split()) > 0) #, regexp="[^А-Яа-яa0-9]+")
@@ -57,7 +91,6 @@ def reply_one_word(message):
     elif text_clean.lower().split()[-1] in ["ага"]:
         bot.reply_to(message, "хуйна")
     
-
     # check if there are more than one word in message 
     if len(text_clean.split()) > 1:
         if "дай мне" in ' '.join(text_clean.lower().split()[-2:]):
@@ -65,5 +98,6 @@ def reply_one_word(message):
         
         elif "будь другом" in ' '.join(text_clean.lower().split()[-2:]):
             bot.reply_to(message, "насри кругом!")
-    
-bot.polling()
+
+if __name__ == '__main__':
+    bot.polling()
